c356185a8e22cbce642755feb0edbf41726016bf
sarriaga35 <sebasarriaga@gmail.com>
2024-06-24 13:21:47 -0600

    Version 0.2, basica, corre en APK24 minimal dialog

--- app/src/main/java/com/sebas/dialogsbasic/MainActivity.kt
+++ app/src/main/java/com/sebas/dialogsbasic/MainActivity.kt
@@ -3,45 +3,66 @@ package com.sebas.dialogsbasic
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
+import androidx.compose.foundation.layout.wrapContentSize
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Card
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.window.Dialog
 import com.sebas.dialogsbasic.ui.theme.DialogsBasicTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
+
         setContent {
             DialogsBasicTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
+               inicialComponible()
             }
         }
     }
-}
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
+    @Composable
+    private fun inicialComponible() {
+        MinimalDialog(hacerAlgo())
 
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    DialogsBasicTheme {
-        Greeting("Android")
+    }
+
+    private fun hacerAlgo(): () -> Unit {
+        return {
+            println("hola")
+        }
+
+    }
+
+    @Composable
+    private fun MinimalDialog(onDimissRequest: () -> Unit) {
+
+        Dialog(onDismissRequest = { onDimissRequest() }) {
+            Card (modifier = Modifier
+                .fillMaxWidth()
+                .height(200.dp)
+                .padding(16.dp),
+                    shape = RoundedCornerShape(16.dp)
+            ) {
+                Text(text = "This is a minimal Dialog",
+                    modifier = Modifier
+                        .fillMaxSize()
+                        .wrapContentSize(Alignment.Center),
+                    textAlign = TextAlign.Center,)
+
+            }
+
+        }
+
     }
 }
+
